70ae7a5edd386b591f5ae0346c5213d45515e36e
salokod <74443387+salokod@users.noreply.github.com>
2024-08-04 18:36:04 -0500

    feat: started add bag functionality

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,18 @@
+import jwt from "jsonwebtoken";
+
 // middleware/auth.js
 export const isAuthenticated = (req, res, next) => {
   // This is a placeholder for authentication logic.
   // In a real-world application, you would check if the user is authenticated here.
-  const userIsAuthenticated = true;
 
-  if (userIsAuthenticated) {
-    next();
-  } else {
-    res.status(401).json({ message: "Unauthorized" });
+  const baboonActToken = req.body.token;
+  try {
+    const decoded = jwt.verify(baboonActToken, process.env.SECRET_TOKEN);
+    if (decoded) {
+      req.jwt = decoded;
+      next();
+    }
+  } catch {
+    return res.status(401).json({ message: "Baboon not logged in" });
   }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 // routes/index.js
-import protectedRoutes from "./protected/index.js";
 import publicRoutes from "./public/index.js";
 import authRoutes from "./public/auth/authRoutes.js";
+import bagRoutes from "./protected/bag/bagRoutes.js";
+
 import { isAuthenticated } from "../middleware/auth.js";
 import express from "express";
 const router = express.Router();
 
-router.use("/secure", isAuthenticated, protectedRoutes);
+router.use("/protected/bag", isAuthenticated, bagRoutes);
 router.use("/public", publicRoutes);
 router.use("/public/auth", authRoutes);
 

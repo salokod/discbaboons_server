5b8b4ece2b3821cd33d40d33298c456e07c1845a
salokod <74443387+salokod@users.noreply.github.com>
2024-08-21 21:29:37 -0500

    getrounds done

---
+++ controllers/protected/rounds/getRoundsController.js
@@ -0,0 +1,16 @@
+import { findAllRounds } from '../../discBaboonUserDataBaseDynamo.js';
+
+const deleteRoundController = async (req, res) => {
+  try {
+    const baboonid = req.jwt.id;
+    const { Items, Count } = await findAllRounds(baboonid);
+
+    return res.status(200).json({ message: 'Rounds retrieved', rounds: { Items, Count } });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: 'Error retrieving rounds, try again later' });
+  }
+};
+
+export default deleteRoundController;
--- routes/protected/rounds/roundRoutes.js
+++ routes/protected/rounds/roundRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import addRoundController from '../../../controllers/protected/rounds/addRoundController.js';
 import deleteRoundController from '../../../controllers/protected/rounds/deleteRoundController.js';
+import getRoundsController from '../../../controllers/protected/rounds/getRoundsController.js';
 
 const router = express.Router();
 
 router.post('/addround', addRoundController);
 router.post('/deleteround', deleteRoundController);
+router.get('/getrounds', getRoundsController);
 
-const endpoints = ['/addround', '/deleteround'];
+const endpoints = ['/addround', '/deleteround', '/getrounds'];
 
 endpoints.forEach((endpoint) => {
   router.all(endpoint, (req, res) => res.status(405).json({ message: 'Method Not Allowed' }));

b6cdc3b48110f7900de0e6f4ae3f386ae081cce9
salokod <74443387+salokod@users.noreply.github.com>
2025-05-03 22:27:14 -0500

    remove unused SQL migration files and add info command to migration script

--- sql/V1__create_users_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_password_change TIMESTAMP
-);
